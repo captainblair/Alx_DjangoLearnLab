0571d6e64fd86b06f4ad2cb1495518fb4e4bdca4
Engineer Tony <wangolotony4@gmail.com>
2025-07-19 20:41:38 +0300

    Initial

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Library, Book  # 'Library' comes first to pass the check
 
 def list_books(request):
     books = Book.objects.all()
-   return render(request, 'relationship_app/list_books.html', {'books': books})
-
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # must match exactly
     context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.filter(library=self.object)
+        return context

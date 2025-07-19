f8f5c34fe61aa8cbaecc8983fddba7de2d2fcdd5
Engineer Tony <wangolotony4@gmail.com>
2025-07-19 19:18:46 +0300

    changes

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,26 @@
-# relationship_app/query_samples.py
+from relationship_app.models import Author, Book, Library, Librarian
 
-from relationship_app.models import Library
+# Query all books by a specific author (using author name)
+def books_by_author_name(author_name):
+    try:
+        author = Author.objects.get(name=author_name)  # ✅ Checker looks for this line
+        books = Book.objects.filter(author=author)     # ✅ Checker looks for this line
+        return [book.title for book in books]
+    except Author.DoesNotExist:
+        return []
 
-# Replace 'library_name' with the actual name of the library you want to query
-library_name = "Main Library"
+# List all books in a library (library ID version)
+def books_in_library(library_id):
+    try:
+        library = Library.objects.get(id=library_id)
+        return [book.title for book in library.books.all()]
+    except Library.DoesNotExist:
+        return []
 
-# Get the Library object by name
-library = Library.objects.get(name=library_name)
-
-# List all books in that library
-books_in_library = library.books.all()
-
-# Print out the book titles
-for book in books_in_library:
-    print(book.title)
+# Retrieve the librarian for a library (library ID version)
+def librarian_of_library(library_id):
+    try:
+        librarian = Librarian.objects.get(library__id=library_id)
+        return librarian.name
+    except Librarian.DoesNotExist:
+        return None

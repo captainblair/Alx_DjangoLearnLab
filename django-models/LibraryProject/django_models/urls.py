1923427fb6200697c5240a302e6355037c732302
Engineer Tony <wangolotony4@gmail.com>
2025-07-20 15:32:29 +0300

    migrations

--- django-models/LibraryProject/django_models/urls.py
+++ django-models/LibraryProject/django_models/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('', include('relationship_app.urls')),  # 👈 include your app here
+    path('', include('relationship_app.urls')),  # this line matters!
 ]
+
 
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.book_list, name='book-list'),
-    path('add/', views.add_book, name='add-book'),
-    path('edit/<int:pk>/', views.edit_book, name='edit-book'),
-    path('delete/<int:pk>/', views.delete_book, name='delete-book'),
+    path('', views.book_list, name='book_list'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
 ]

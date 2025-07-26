b8cc5ba0a4d70257d285e7aaaed90c84eb25552e
Engineer Tony <wangolotony4@gmail.com>
2025-07-26 17:50:18 +0300

    Errors have been checked

--- advanced_features_and_security/users/models.py
+++ advanced_features_and_security/users/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+# Custom User Model
+class CustomUser(AbstractUser):
+    username = None  # we don’t use username
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # removes username field from required
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email

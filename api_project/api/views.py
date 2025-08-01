c00deae65a6c59a7b189b0887271cb7da0c9dde3
Engineer Tony <wangolotony4@gmail.com>
2025-08-01 19:42:45 +0300

    CRUD operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,23 @@
 # api/views.py
 
-from rest_framework import generics
+# Add 'viewsets' to this import
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# This is the old view from the previous task
 class BookList(generics.ListAPIView):
     """
     API view to retrieve a list of all books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Add this new ViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing book instances.
+    Provides `list`, `create`, `retrieve`, `update`, and `destroy` actions.
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer

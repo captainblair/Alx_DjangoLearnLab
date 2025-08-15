e7d8cb5e241d01db88d25ffde7109e4562dbc18c
Engineer Tony <wangolotony4@gmail.com>
2025-08-16 00:20:22 +0300

    add a model and admin

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,21 +1,11 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Post
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(required=False)
-    last_name = forms.CharField(required=False)
-
+class PostForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = [
-            'username', 'email', 'first_name', 'last_name', 'password1', 'password2'
-        ]
-
-class ProfileUpdateForm(forms.ModelForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email']
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Post title'}),
+            'content': forms.Textarea(attrs={'rows': 8, 'placeholder': 'Write your post here...'}),
+        }
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,31 +1,47 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-from django.contrib.auth.models import User
-from django import forms
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+from .forms import PostForm
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    class Meta:
-        model = User
-        fields = ["username", "email", "password1", "password2"]
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # blog/templates/blog/post_list.html
+    context_object_name = 'posts'
+    paginate_by = 5
 
-def register(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Account created successfully!")
-            return redirect('login')
-    else:
-        form = RegisterForm()
-    return render(request, 'blog/register.html', {'form': form})
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        request.user.email = request.POST.get('email', request.user.email)
-        request.user.save()
-        messages.success(request, "Profile updated!")
-    return render(request, 'blog/profile.html', {'form': None})
+class PostCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user

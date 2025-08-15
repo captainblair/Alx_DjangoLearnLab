9039e469fdf25d0181400136411c89bcd13336f7
Engineer Tony <wangolotony4@gmail.com>
2025-08-16 00:22:19 +0300

    changes

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,33 +1,27 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-from .forms import PostForm
 
-class PostListView(generic.ListView):
+class PostListView(ListView):
     model = Post
-    template_name = 'blog/post_list.html'  # blog/templates/blog/post_list.html
+    template_name = 'blog/post_list.html'  # adjust as needed
     context_object_name = 'posts'
-    paginate_by = 5
+    ordering = ['-date_posted']
 
-class PostDetailView(generic.DetailView):
+class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
 
-class PostCreateView(LoginRequiredMixin, generic.CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -35,13 +29,12 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView
 
     def test_func(self):
         post = self.get_object()
-        return post.author == self.request.user
+        return self.request.user == post.author
 
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post-list')
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
-        return post.author == self.request.user
+        return self.request.user == post.author
